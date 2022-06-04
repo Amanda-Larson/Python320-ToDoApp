2e111465563790a49a876c79aa11d0e2aa1b1806
Amanda-Larson <larson.amandarae@gmail.com>
2022-06-04 13:34:37 -0700

    commit again

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/_")
+module_dir = os.path.join(__location__, "../src/ToDoListApp")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
